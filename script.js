b42e591ac86b8156284f10238bb6fbbc63755d6a
David <dakinola54@gmail.com>
2024-02-12 03:47:14 -0800

    removed transaction property

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const userProfile = {
-    userName: 'David Akinola',
-    email: 'dakinola54@gmail.com',
-    password: 123456,
-    profilePicture: 'dave.jpeg',
+// const userProfile = {
+//     userName: 'David Akinola',
+//     email: 'dakinola54@gmail.com',
+//     password: 123456,
+//     profilePicture: 'dave.jpeg',
 
-    changePassword: function(newPassword) {
-        this.password = newPassword
-    },
+//     changePassword: function(newPassword) {
+//         this.password = newPassword
+//     },
 
-    changeProfilePicture: function(newPhoto) {
-        this.profilePicture = newPhoto
-    }
-};
+//     changeProfilePicture: function(newPhoto) {
+//         this.profilePicture = newPhoto
+//     }
+// };
 
-console.log(userProfile)  //outputs initial object
+// console.log(userProfile)  //outputs initial object
 
-userProfile.changePassword(56789);     //changes password
-userProfile.changeProfilePicture('new.jpeg');  //changes profile photo 
+// userProfile.changePassword(56789);     //changes password
+// userProfile.changeProfilePicture('new.jpeg');  //changes profile photo 
 
-console.log(userProfile); //outputs new object
+// console.log(userProfile); //outputs new object
 
 
 
@@ -29,8 +29,6 @@ console.log(userProfile); //outputs new object
 const bankingAccount = {
     accountNumber: 1234567899,
     balance: 2500,
-    teansactionHistory: '',
-
 
     withdrawFund: function(amount) {
         this.balance -= amount
@@ -47,5 +45,5 @@ console.log(bankingAccount);
 bankingAccount.withdrawFund(1000);   //withdraws ammount from balance
 console.log('Withdrew Fund:', bankingAccount);
 
-bankingAccount.depositeFund(3000)   //deposite ammount to balance
-console.log('Deposited fund:', bankingAccount);
+// bankingAccount.depositeFund(3000)   //deposite ammount to balance
+// console.log('Deposited fund:', bankingAccount);
