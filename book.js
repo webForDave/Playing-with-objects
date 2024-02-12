1402950fed9f704cdca91fc3e33f5952db37139c
David <dakinola54@gmail.com>
2024-02-12 04:35:45 -0800

    building new book object

---
+++ book.js
@@ -0,0 +1,14 @@
+const Book = class{
+    constructor(
+        bookTitle,
+        bookPubYear,
+        bookAuthor,
+        bookISBN
+    ) {
+
+        this.bookTitle = bookTitle,
+        this.bookPubYear = bookPubYear,
+        this.bookAuthor = bookAuthor,
+        this.bookISBN = bookISBN
+    }
+}
