c2253953de5f71a8beb3cb5a8774d0d069469320
David <dakinola54@gmail.com>
2024-02-11 17:56:54 -0800

    building methods

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const userProfile = {
     userName: 'David Akinola',
     email: 'dakinola54@gmail.com',
     password: 123456,
-    profilePicture: 'dave.jpeg'
+    profilePicture: 'dave.jpeg',
 
-    
+    changePawword: function(newPassword) {
+        this.password = newPassword
+    },
+
+    changeProgilePicture: function(newPhoto) {
+        this.profilePicture = newPhoto
+    }
 }
